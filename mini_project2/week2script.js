2138d8edca07a309e5e109a8d9b3c96103c2c53d
farheenbano9219-dev <farheenbano9219@gmail.com>
2026-04-07 20:24:08 +0530

    mini project2

---
+++ mini_project2/week2script.js
@@ -0,0 +1,30 @@
+const apiKey = "c4273816f7d9882c5a4c7e8be5bbe7f3"; // Put your API key here
+
+async function getWeather() {
+    const city = document.getElementById("city").value;
+    const url = 'https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric;'
+
+    try {
+        const response = await fetch(url);
+
+        if (!response.ok) {
+            throw new Error("City not found");
+        }
+
+        const data = await response.json();
+
+        document.getElementById("weatherBox").classList.remove("hidden");
+
+        document.getElementById("cityName").innerText = data.name;
+        document.getElementById("temp").innerText = "Temperature: " + data.main.temp + "°C";
+        document.getElementById("condition").innerText = "Condition: " + data.weather[0].main;
+        document.getElementById("humidity").innerText = "Humidity: " + data.main.humidity + "%";
+        document.getElementById("wind").innerText = "Wind Speed: " + data.wind.speed + " m/s";
+
+        document.getElementById("error").innerText = "";
+
+    } catch (error) {
+        document.getElementById("weatherBox").classList.add("hidden");
+        document.getElementById("error").innerText = error.message;
+    }
+}
